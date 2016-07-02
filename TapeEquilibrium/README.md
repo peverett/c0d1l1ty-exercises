bbd3881f089d066f2dddd883d7a16f5c54b4bc50
peverett <simon.peverett@gmail.com>
2016-07-02 16:16:28 +0100

    Add TapeEquilibrium exercise, Lesson 3

---
+++ TapeEquilibrium/README.md
@@ -0,0 +1,53 @@
+# c0d1l1ty Lesson 3: Time Complexity
+## Exercise 2: [TapeEquilibrium](https://codility.com/programmers/task/tape_equilibrium/)
+
+Given a non-empty zero-indexed array A, at any index that splits the array in
+two, find the difference between those two parts and return the minimum
+difference found.
+
+Example:
+    - A = [3, 1, 2, 4, 3]
+    - Differences:
+        * p = 1, difference = | 3 - 10 | = 7
+        * p = 2, difference = | 4 - 9 | = 5
+        * p = 3, difference = | 6 - 7 | = 1
+        * p = 4, difference = | 10 - 3| = 7
+     - The lowest difference is 1.
+
+Hints:
+    - Worst-case time complexity is O(N) so the calculation must be done in
+    a single loop.
+    - Worst-case space complexity is O(N) + input storage.
+
+##My solution
+
+1. Calculate the sum of the whole array = right_sum.
+2. loop through N elements:
+    1. left_sum += current element
+    2. right_sum -= current element
+    2. difference = absolute(left_sum - right_sum);
+    3. If lowest difference, store it.
+3. Return the lowest stored difference.
+
+Note, initialise 'lowest_difference' to something very high.
+
+## Test Cases
+
+1. Example provided
+    - Inputs: A = [3, 1, 2, 4, 3], N = 5
+    - Expected:  1
+2. Maximum N, of random elements range -1000..1000.
+    - Inputs: A = [?, ?, ?, ... ?], N = 100000
+    - Expected:  ?
+3. Maximum N, each element is 1000.
+    - Inputs: A = [1000, 1000, 1000, ... 1000], N=100000
+    - Expected:  0
+
+## Score
+This solution scored:
+ - 100% for correctness.
+ - 100% for required performance, which is O(N).
+
+This solution iterates through the whole array twice. Once to calculate
+the sum of the entire array, and then again to find the lowest difference.
+I'm not sure if this is really 0(N) performance.
---
+++ TapeEquilibrium/main.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int solution(int A[], int N)
+{
+    int difference;
+    int lowest_difference = 1000 * 100000;
+    int right_sum = 0;
+    int left_sum = 0;
+
+    /* Calculate the sum of the entire array. */
+    for (int i=0; i<N; i++) right_sum += A[i];
+
+    /* Now loop through, recalculating left and right as we go and
+     * store the lowest difference.
+     */
+    for (int i=0; i<N-1; i++) {
+        left_sum += A[i];
+        right_sum -= A[i];
+        difference = abs(right_sum - left_sum);
+        if (abs(right_sum - left_sum) < lowest_difference)
+            lowest_difference = difference;
+    }
+    return lowest_difference;
+}
+
+#define MIN(X,Y) (((X)<(Y)) ? (X) : (Y))
+void test_case(int A[], int N, int exp_res)
+{
+    int res = solution(A, N);
+    int max = MIN(N, 7);
+
+    printf("\nInput: A = [ ");
+
+    for (int i=0; i<max; i++) printf("%d ", A[i]);
+    if (max == 7) {
+        printf("... %d ", A[N-1]);
+    }
+    printf("], N=%d\n", N);
+    printf("Result: %d\n", res);
+
+    if (exp_res >= 0) {
+        printf("Expected: %d\n", exp_res);
+        printf("%s\n", (res == exp_res) ? "PASS" : "FAIL");
+    }
+}
+
+int main(void)
+{
+    const int max = 100000;               // N = 2..100,000
+    int test0[] = { 3, 1, 2, 4, 3 };
+    int test1[max];
+    int test2[max];
+
+    // Initialise array with maximum N elements, random elements in range.
+    // -1000 to 1000.
+    for (int i=0; i<max; i++) {
+        test1[i] = (rand() % 2001) - 1000;
+    }
+
+    // Initialise the array with maximum N elements, all 1000.
+    for (int i=0; i<max; i++) {
+        test2[i] = 1000;
+    }
+
+    printf("TapeEquilibrium\n");
+    test_case(test0, sizeof(test0)/sizeof(int), 1);
+    test_case(test1, max, -1);
+    test_case(test2, max, 0);
+
+    return 0;
+}
+
