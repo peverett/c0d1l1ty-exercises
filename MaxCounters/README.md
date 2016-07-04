7343feddc42d8582128243c9cdd18654f58b1962
peverett <simon.peverett@gmail.com>
2016-07-04 11:46:13 +0100

    Add solution to Lesson 5, Exercise 4: MaxCounters.

---
+++ MaxCounters/README.md
@@ -0,0 +1,75 @@
+# c0d1l1ty Lesson 4: Counting Elements
+## Exercise 4: [MaxCounters](https://codility.com/programmers/task/max_counters/)
+
+Write a function:
+```
+    struct Results solution(int N, int A[], int M);
+```
+where:
+- N is the number of counters from 1 to N.
+- A[] is a non-empty zero-indexed array containing integers in the range 1..N+1.
+- M is the number of elements in A.
+
+For each integer in A perfrom one of the two possible operations:
+- if 1<= A[K] <=N then increase the counter for that value.
+- if A[K] = N+1 then make all counters the maximum counter value.
+
+Example:
+- Input: N = 5, A = [3, 4, 4, 6, 1, 4, 4], M = 7
+- Expected operation:
+    * A[0] = 3, counters = (0, 0, 1, 0, 0) // Inc counter 3
+    * A[1] = 4, counters = (0, 0, 1, 1, 0) // Inc counter 4
+    * A[2] = 4, counters = (0, 0, 1, 2, 0) // Inc counter 4
+    * A[3] = 6, counters = (2, 2, 2, 2, 2) // Max all counters to 2
+    * A[4] = 1, counters = (3, 2, 2, 2, 2) // Inc counter 1
+    * A[5] = 4, counters = (3, 2, 2, 3, 2) // Inc counter 4
+    * A[6] = 4, counters = (3, 2, 2, 4, 2) // Inc counter 4
+- Expected: Results { C = [3, 2, 2, 4, 2], L = 5 }
+
+Expected worst-case time complexity is O(N+M).
+
+##My solution
+
+The expected worst-case time complexity of O(N+M) indicates that the solution
+should use a single iteration across the input array A and the counter array
+result.C.
+
+The trick to achieving this is to use a form of (lazy initialisation)[https://en.wikipedia.org/wiki/Lazy_initialization];
+Only apply the Max Counter when is is greater than the current counter value.
+After the input array A has been processed, apply the Max Counter to all
+elements that are less than that value.
+
+1. Allocate and initialise the Results struct.
+2. Loop through all elements of A
+    * If current element == N + 1, set Highest Count so far to Max Count.
+    * else:
+        1. If counter for current element is < Max Count, set it to Max Count.
+        2. Increment this counter.
+        3. If this counter is the highest so far, store the Highest Count.
+3. Loop through all elements of the counters.
+    * If current element < Max Count, set it to Max Count.
+4. Return Results struct.
+
+## Test Cases
+
+1. Minimal inputs
+    * Inputs: N = 1, A = [ 1 ], M = 1
+    * Expected: Results { C = [1], L = 1 }
+2. Example
+    * Inputs: N = 5, A = [ 3, 4, 4, 6, 1, 4, 4 ], M = 7
+    * Expected: Results { C = [ 3, 2, 2, 4, 2 ], L = 5 }
+3. Only max counters
+    * Inputs: N = 5, A = [ 6, 6, 6, 6, 6, 6, 6 ], M = 7
+    * Expected: Results { C = [ 0, 0, 0, 0, 0 ], L = 5 }
+4. Two max counters
+    * Inputs: N = 5, A = [ 3, 4, 4, 6, 1, 6, 4 ], M = 7
+    * Expected: Results { C = [ 3, 3, 3, 4, 3 ], L = 5 }
+5. Maximum values
+    * Inputs: N = 100000, A = [ rand(1..100001) * M ], M = 100000
+    * Expected: Results { C = [? ... ?], L = 100000 }
+
+## Score
+
+This solution scored:
+ - 100% for correctness.
+ - 100% for required performance, which is O(N+M).
--- MaxCounters/main.c
+++ MaxCounters/main.c
@@ -1,8 +1,146 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+struct Results {
+    int *C;
+    int L;
+};
+
+struct Results solution(int N, int A[], int M) {
+    struct Results result;
+    int max_count = 0;
+    int highest_count = 0;
+
+    // Allocate and initialise results
+    result.C = (int *)calloc(N, sizeof(int));
+    result.L = N;
+
+    printf("Intialised\n");
+
+    // Loop through all elements of array A
+    for (int i=0; i<M; i++) {
+        // If current element == N+1
+        if (A[i] == N+1) {
+            // Set the Max Count to the Highest Count so far.
+            max_count = highest_count;
+        }
+        else {
+            // If the count for the Current Element is less than the
+            // Max Count so far
+            if (result.C[A[i]-1] < max_count)
+                // Set the count to the Max Count
+                result.C[A[i]-1] = max_count;
+            // Increment the count for the Current Element
+            result.C[A[i]-1]++;
+            // If the count for the Current Element is the Highest Count so far
+            if (result.C[A[i]-1] > highest_count)
+                // Store it as the Highest Count
+                highest_count = result.C[A[i]-1];
+        }
+    }
+    printf("Processed\n");
+
+    // Loop through all the counters
+    for (int i=0; i<N; i++) {
+        // If the current counter is less than the Max Count
+        if (result.C[i] < max_count)
+            // Make the counter equal to the Max Count.
+            result.C[i] = max_count;
+    }
+
+
+    return result;
+}
+
+#define MIN(X,Y) (((X)<(Y)) ? (X) : (Y))
+void print_array( char array_legend, int arr[], char len_legend, int len)
+{
+    int max = MIN(len, 8);
+
+    printf("%c = [ ", array_legend);
+    for (int i=0; i<max; i++) printf("%d ", arr[i]);
+    if (max == 8) {
+        printf("... %d ", arr[len-1]);
+    }
+    printf("], %c = %d\n", len_legend, len);
+}
+
+void test_case(int N, int A[], int M, struct Results *exp_res)
+{
+    struct Results res; // = solution(N, A, M);
+    int fail = 0;
+
+    printf("\nInput: N = %d, ", N);
+    print_array( 'A', A, 'M', M);
+
+    res = solution(N, A, M);
+
+    printf("Result: ");
+    print_array( 'C', res.C, 'L', res.L);
+
+    if (exp_res) {
+        printf("Expected: ");
+        print_array('C', exp_res->C, 'L', exp_res->L);
+
+        if (exp_res->L != res.L)
+            fail = 1;
+        else {
+            for(int i=0; i< res.L; i++) {
+                if (exp_res->C[i] != res.C[i]) {
+                    fail = 1;
+                    break;
+                }
+            }
+        }
+        printf("%s\n", (fail) ? "FAIL" : "PASS");
+    }
+}
 
 int main(void)
 {
-    printf("Hello World!\n");
+    const int max = 100000;             // N = M = 1..100,000
+
+    // N = 1, A = [1], M = 1
+    int test0[] = { 1 };
+    int res0[] = { 1 };
+    struct Results exp_res0 = { res0, 1 };
+
+    // Exercise example
+    int test1[] = { 3, 4, 4, 6, 1, 4, 4 };
+    int res1[] = { 3, 2, 2, 4, 2 };
+    struct Results exp_res1 = { res1, 5 };
+
+    // All max counters
+    int test2[] = { 6, 6, 6, 6, 6, 6, 6 };
+    int res2[] = { 0, 0, 0, 0 , 0 };
+    struct Results exp_res2 = { res2, 5 };
+
+    // Two Max Counters
+    int test3[] = { 3, 4, 4, 6, 1, 6, 4 };
+    int res3[] = { 3, 3, 3, 4, 3 };
+    struct Results exp_res3 = { res3, 5 };
+
+    // Maximum values
+    int testmax[max];
+    for (int i=0; i<max; i++) {
+        testmax[i] = (rand() % max) + 1;   // 1..100001.
+    }
+
+    printf("MaxCounters\n");
+
+    // N = 1, A = [1], M = 1
+    test_case(1, test0, 1, &exp_res0);
+
+    // Exercise example
+    test_case(5, test1, 7, &exp_res1);
+
+    // All max counters
+    test_case(5, test2, 7, &exp_res2);
+
+    // Two max counters
+    test_case(5, test3, 7, &exp_res3);
+
+    // Maximum values
+    test_case(max, testmax, max, NULL);
     return 0;
 }
-
