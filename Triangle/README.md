955fe26aff235d8cb1a9b90e9cd99f7f01d77a09
peverett <simon.peverett@gmail.com>
2016-07-10 09:34:00 +0100

    Lesson 6: Sorting, Exercise 1: Triangle.

---
+++ Triangle/README.md
@@ -0,0 +1,81 @@
+# cod1l1ty Lesson 6: Sorting
+## Exercise 1: [Triangle](https://codility.com/programmers/task/triangle/)
+
+A zero-indexed array A consisting of N integers is given. A truplet (P, Q, R) is
+a triangular if:
+- 0 <= P < Q < R < N
+- A[P] + A[Q] > A[R]
+- A[Q] + A[R] > A[P]
+- A[R] + A[P] > A[Q]
+
+Write a function that given a zero-indexed array A consisting of N integers,
+returns 1 if there exists a triangular triplet for this array and return 0
+otherwise.
+
+Example:
+- Inputs: A = [10, 2, 5, 1, 8, 20], N = 6
+- Expected: Triplet (0, 2, 4) is triangular, should return 1.
+
+Hints:
+- Expected worst-case time complexity is O(N*log(N)); So a sort is required in
+the solution.
+
+## My solution
+
+I expected a trick math question, but, suprisingly, I worked this one out
+myself. Still took a couple of goes to get 100% though.
+
+1. Remove any value from array A that < 1 and recalculate N.
+2. If N < 3 return 0, can't be a triangle with less than 3 sides.
+3. Sort the array A.
+4. Loop for P through array A from 0 to N-3
+    1. Loop for Q through array A from P to N-2
+        1 R = Q+1
+        2 if A[P] + A[Q] > A[R] return 1
+5. If the loops complete, there is no triangle, return 0
+
+How does this work?
+* If we sort the values into progressive sequence, we know that P <= Q <= R,
+  therefore, only the 'P + Q > R' test is needed.
+* The simplest solution is that whatever value of P, if Q and R are the same,
+  then this is an isosceles triangle, so you could first check if `Q == R`, but
+  the `P + Q > R` rule still applies so just use that.
+* For C, use the `long long` type for the A[P] + A[Q] operation, because at the
+  maximum positive range of values in A this addition will cause an overflow
+  otherwise.
+
+## Test Cases
+
+1. Default, empty array
+    * Inputs: A = [], N = 0
+    * Expected: 0
+2. Only two elements
+    * Inputs: A = [1, 2], N = 2
+    * Expected: 0
+3. Three elements, is a triangle
+    * Inputs: A = [5, 8, 4], N = 3
+    * Expected: 1
+4. Three elements, not a triangle
+    * Inputs: A = [8, 5, 2], N = 0
+    * Expected: 0
+5. Example
+    * Inputs: A = [10, 2, 5, 1, 8, 20], N = 6
+    * Expected: 1
+6. Three values, all max value of 2147483647
+    * Inputs: A = [2147483647, 2147483647, 2147483647], N = 3
+    * Expected: 1
+7. Extreme ranges, triangle
+    * Inputs: A = [2, 2147483646, 2147483647], N = 3
+    * Expected: 1
+8. Maximum N (N=100000), random values of A
+    * Inputs: A = [?, ?, ?, ... ?], N = 100000
+    * Expected: ?
+9. Maximum N (N=100000), all values of A are negative.
+    * Inputs: A = [-1, -1, -1, ... -1], N = 100000
+    * Expected: 0
+
+## Score
+
+My solution scored 100% overall:
+ - 100% for correctness.
+ - 100% for required performance.
--- Triangle/main.c
+++ Triangle/main.c
@@ -1,8 +1,127 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+int comp(const void *left, const void *right)
+{
+    int l = *((int*)left);
+    int r = *((int*)right);
+    if (r < l) return 1;
+    if (r > l) return -1;
+    return 0;
+}
+
+int solution(int A[], int N)
+{
+    // Remove values < 1 from A.
+    int x = 0;
+    for (int i=0; i<N; i++) {
+        if (A[i] < 1) continue;
+        else A[x++] = A[i];
+    }
+    N = x;
+
+    // At least three elements needed.
+    if (N < 3) return 0;
+
+    // Sort the input array
+    qsort(A, N, sizeof(int), comp);
+
+    // P for all elements of A to N-2
+    for(int P=0; P<N-2; P++) {
+        long long p = A[P];
+        // Q from P+1 to N-1;
+        for (int Q=P+1; Q < N-1; Q++) {
+            long long q = A[Q];
+            // R is Q + 1
+            long long r = A[Q+1];
+            // If P + Q > R, this is a triangle, return 1.
+            if (r < (q + p)) return 1;
+        }
+    }
+    return 0;
+}
+
+#define MIN(X,Y) (((X)<(Y)) ? (X) : (Y))
+void print_array( char array_legend, int arr[], char len_legend, int len)
+{
+    int max = MIN(len, 8);
+
+    printf("%c = [ ", array_legend);
+    for (int i=0; i<max; i++) printf("%d ", arr[i]);
+    if (max == 8) {
+        printf("... %d ", arr[len-1]);
+    }
+    printf("], %c = %d\n", len_legend, len);
+}
+
+void triangle(int N)
+{
+    // All the valid numbers from 1 to N that make a triangle
+    for (int p=1; p<=N; p++) {
+        for (int q=p; q<=N; q++) {
+            for (int r=q; r<=N; r++) {
+                if (p+q > r && q+r > p && r+p > q)
+                    printf("P = %d, Q = %d, R = %d\n", p, q, r);
+            }
+        }
+    }
+}
+
+
+void test_case(int A[], int N, int exp_res)
+{
+    int res;
+
+    printf("\nInputs: ");
+    print_array('A', A, 'N', N);
+
+    res = solution(A, N);
+    printf("Result: %d\n", res);
+
+    if (exp_res >= 0) {
+        printf("%s\n", (exp_res == res) ? "PASS" : "FAIL");
+    }
+}
 
 int main(void)
 {
-    printf("Hello World!\n");
+    printf("Triangle\n");
+
+    const int max = 100000;
+    int test0[] = {};
+    int test1[] = {1, 2};
+    int test2[] = {5, 8, 4};
+    int test3[] = {5, 8, 2};
+    int test4[] = {10, 2, 5, 1, 8, 20};
+    int test5[] = {2147483647, 2147483647, 2147483647};
+    int test6[] = {2, 2147483646, 2147483647};
+    int testmax[max];
+
+    //triangle(10);
+
+    test_case(test0, sizeof(test0)/sizeof(int), 0);
+    test_case(test1, sizeof(test1)/sizeof(int), 0);
+    test_case(test2, sizeof(test2)/sizeof(int), 1);
+    test_case(test3, sizeof(test3)/sizeof(int), 0);
+    test_case(test4, sizeof(test4)/sizeof(int), 1);
+    test_case(test5, sizeof(test5)/sizeof(int), 1);
+    test_case(test6, sizeof(test6)/sizeof(int), 1);
+
+    // Max N (N=100000), random values in 32-bit range.
+    for (int i=0; i<max; i++) {
+        int val = rand();
+        if ((rand() % 2)) {
+            val = 0 - val;
+        }
+        testmax[i] = val;
+    }
+    test_case(testmax, max, -1);
+
+    // Max M (N=100000), all negative
+    for (int i=0; i<max; i++) {
+        testmax[i] = -1;
+    }
+    test_case(testmax, max, -1);
     return 0;
 }
 
